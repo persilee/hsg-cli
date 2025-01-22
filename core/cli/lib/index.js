235f282b686d6da25e896e26485c77c37c137cf9
persilee <i@lishaoy.net>
2025-01-22 17:06:01 +0800

    🐛 fix code

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -5,6 +5,7 @@ module.exports = core
 const pkg = require('../package.json')
 const log = require('@hsg-cli/log')
 const constant = require('./const')
+const semver = require('semver')
 
 function core() {
   checkPkgVersion()
@@ -12,8 +13,16 @@ function core() {
 }
 
 function checkNodeVersion() {
-  const currentVersion = process.version
-  const lowestVersion = constant.LOWEST_NODE_VERSION
+  try {
+    const currentVersion = process.version
+    const lowestVersion = constant.LOWEST_NODE_VERSION
+    if (!semver.gte(currentVersion, lowestVersion)) {
+      throw new Error(`Node.js version ${lowestVersion} or higher is required. Current version: ${currentVersion}`)
+    }
+  } catch (error) {
+    log.error('Node.js Version Check', error.message)
+    process.exit(1)
+  }
 }
 
 function checkPkgVersion() {
